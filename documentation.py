0335d89ff86bf21686280d7a894379f2b476bd02
eefjonkheer <81562483+eefjonkheer@users.noreply.github.com>
2021-03-31 11:29:32 +0200

    Update documentation.py

--- documentation.py
+++ documentation.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+
+"""
+Read an Excel file and return the headers 
+
+Args: 
+   a, string, the path to an Excel file
+   print_columns, boolean, print the column headers during the retrieval (default is False)
+   
+Returns: 
+ column_headers, a list with the column headers
+
+"""
 def x(a, print_columns=False):
    b = pd.read_excel(a)
    column_headers = list(b.columns.values)
